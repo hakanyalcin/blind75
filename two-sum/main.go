ecd0728c7b52af92d70a14ee78872e537a204134
Hakan Yalçın <hakan@svexa.com>
2024-04-18 20:23:15 +0300

    two-sum v1

---
+++ two-sum/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{2, 7, 11, 15}
+	target := 9
+
+	fmt.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	i, j := 0, 0
+	res := []int{}
+	for i = 0; i < len(nums); i++ {
+		for j = i + i; j < len(nums)-1; j++ {
+			if nums[i]+nums[j] == target {
+				res = append(res, nums[i])
+				res = append(res, nums[j])
+				return res
+			}
+		}
+	}
+	return res
+}
