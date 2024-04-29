d12fcfe5b8c1e16e98fb93306fe9df6717a53bdd
Hakan Yalçın <hakan@svexa.com>
2024-04-29 23:41:52 +0300

    max profit n time solution

---
+++ max-profit/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	//prices := []int{7, 1, 5, 3, 6, 4}
+	//prices := []int{7, 6, 4, 3, 1}
+	prices := []int{2, 1, 2, 0, 1}
+
+	fmt.Println(maxProfit(prices))
+}
+
+func maxProfit(prices []int) int {
+	var profit = 0
+	var minPrice = prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if (prices[i] - minPrice) > profit {
+			profit = prices[i] - minPrice
+		}
+	}
+
+	return profit
+}
