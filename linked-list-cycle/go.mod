aeabd1a07e79683f86b4978a2b64f094bc2da3ba
Hakan Yalçın <hakan@svexa.com>
2024-06-25 00:24:32 +0300

    linked-list-cycl detect solved

---
+++ linked-list-cycle/go.mod
@@ -0,0 +1,3 @@
+module ll-cycle
+
+go 1.22.2
---
+++ linked-list-cycle/main.go
@@ -0,0 +1,25 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func main() {
+
+}
+
+func hasCycle(head *ListNode) bool {
+	slow_pointer, fast_pointer := head, head
+	for fast_pointer != nil && fast_pointer.Next != nil {
+		slow_pointer = slow_pointer.Next
+		fast_pointer = fast_pointer.Next.Next
+		if slow_pointer == fast_pointer {
+			return true
+		}
+	}
+	return false
+}
